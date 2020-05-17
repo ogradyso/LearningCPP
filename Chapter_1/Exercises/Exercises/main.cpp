5f0a970df6a917faa9f5cfabb0cef64cfe29f736
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2020-05-17 08:30:09 -0500

    Completed Exercises

--- Chapter_1/Exercises/Exercises/main.cpp
+++ Chapter_1/Exercises/Exercises/main.cpp
@@ -5,9 +5,15 @@ int absoluteValue(int inputVal) {
 	else return inputVal * -1;
 }
 
+int sum(int val1, int val2) {
+	return val1 + val2;
+}
+
 int main() {
 	int valToPass = -42;
 	int absValToPass = absoluteValue(valToPass);
-	printf("The absolute value of %d is %d.", valToPass, absValToPass);
+	int sumFun = sum(valToPass, absValToPass);
+	printf("The absolute value of %d is %d.\n", valToPass, absValToPass);
+	printf("The sum of both integers is %d.", sumFun);
 	return 0;
 }
