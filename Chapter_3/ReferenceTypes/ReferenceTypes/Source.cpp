c4c4e50b5d68f6eb5a012c38cd043c5c14702016
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2020-05-21 19:04:07 -0500

    Forward linked lists, and more pointer stuff.

--- Chapter_3/ReferenceTypes/ReferenceTypes/Source.cpp
+++ Chapter_3/ReferenceTypes/ReferenceTypes/Source.cpp
@@ -33,6 +33,18 @@ void add_year(ClockOfTheLongNow& clock) {
 	clock.set_year(clock.get_year() + 1); // no dereference operatore needed
 }
 
+//forward linked lists
+struct Element {
+	Element* next{};
+	void insert_after(Element* new_element) {
+		new_element->next = next;
+		next = new_element;
+	}
+	char prefix[2];
+	short operating_number;
+
+};
+
 int main() {
 	//pointers
 	// pointers encode an objects type and its address
@@ -108,5 +120,39 @@ int main() {
 	printf("The year is %d.\n", clock2.get_year());
 	add_year(clock2); // clock is implicitly passed by reference
 	printf("The year is %d.\n", clock2.get_year());
+
+	//pointers and forward linked lists
+	Element trooper1, trooper2, trooper3;
+	trooper1.prefix[0] = 'T';
+	trooper1.prefix[1] = 'K';
+	trooper1.operating_number = 421;
+	trooper1.insert_after(&trooper2);
+	trooper2.prefix[0] = 'F';
+	trooper2.prefix[1] = 'N';
+	trooper2.operating_number = 2187;
+	trooper2.insert_after(&trooper3);
+	trooper3.prefix[0] = 'L';
+	trooper3.prefix[1] = 'S';
+	trooper3.operating_number = 005;
+
+	for (Element* cursor = &trooper1; cursor; cursor = cursor->next) {
+		printf("stormtrooper %c%c-%d\n",
+			cursor->prefix[0],
+			cursor->prefix[1],
+			cursor->operating_number);
+	}
+
+	//employing references:
+	//setting reference reseats a value
+	int original = 100;
+	int& original_ref = original;
+	printf("Original: %d\n", original);
+	printf("Original_ref: %d\n", original_ref);
+
+	int new_value = 200;
+	original_ref = new_value;
+	printf("Original: %d\n", original);
+	printf("new_value: %d\n", new_value);
+	printf("original_ref: %d\n", original_ref);
 
 }
