57f31fbf7ae21badedbb66e8e3ee99dd989439f3
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2020-05-22 20:27:57 -0500

    Doubly linked lists

--- Chapter_3/Exercises/Chapter3Exercises/Chapter3Exercises/Source.cpp
+++ Chapter_3/Exercises/Chapter3Exercises/Chapter3Exercises/Source.cpp
@@ -17,18 +17,23 @@ struct Element {
 	Element* next{};
 	Element* previous{};
 	void insert_after(Element* new_element) {
-		new_element->next = next;
-		next = new_element;
+		new_element->next = this->next;
+		new_element->previous = this;
+		this->next = new_element;
 	}
 	void insert_before(Element* new_element) {
-		new_element->previous = previous;
-		previous = new_element;
+		new_element->previous = this->previous;
+		new_element->next = this;
+		(this->previous)->next = new_element;
+		this->previous = new_element;
 	}
 	char prefix[2];
 	short operating_number;
 };
 
 int main() {
+
+	//Exercise 2:
 	char lower[] = "abc?e";
 	char upper[] = "ABC?E";
 	char* upper_ptr = upper;
@@ -42,7 +47,39 @@ int main() {
 	printf("lower: %s\nupper: %s\n", lower, upper);
 	char read_letter = read_from(lower, 2);
 	printf("Testing read_from: %c\n", read_letter);
-	write_to(upper,3, 'f');
+	write_to(upper, 3, 'f');
 	write_to(upper, 10, '5');
+
+	//Exercise 3:
+	Element trooper1, trooper2, trooper3, trooper4;
+	trooper1.prefix[0] = 'T';
+	trooper1.prefix[1] = 'K';
+	trooper1.operating_number = 421;
+	trooper1.insert_after(&trooper2);
+	trooper2.prefix[0] = 'F';
+	trooper2.prefix[1] = 'N';
+	trooper2.operating_number = 2187;
+	trooper2.insert_after(&trooper3);
+	trooper3.prefix[0] = 'L';
+	trooper3.prefix[1] = 'S';
+	trooper3.operating_number = 005;
+	trooper3.insert_before(&trooper4);
+	trooper4.prefix[0] = 'S';
+	trooper4.prefix[1] = 'O';
+	trooper4.operating_number = 42;
+
+	for (Element* cursor = &trooper1; cursor; cursor = cursor->next) {
+		printf("stormtrooper %c%c-%d\n",
+			cursor->prefix[0],
+			cursor->prefix[1],
+			cursor->operating_number);
+	}
+	for (Element* cursor = &trooper3; cursor; cursor = cursor->previous) {
+		printf("stormtrooper %c%c-%d\n",
+			cursor->prefix[0],
+			cursor->prefix[1],
+			cursor->operating_number);
+	}
+
 
 }
