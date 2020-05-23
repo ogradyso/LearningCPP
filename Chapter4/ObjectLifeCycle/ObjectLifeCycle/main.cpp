99482f3844c283fea69eda13259b532a1d4fbf9e
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2020-05-23 08:50:48 -0500

    static variables and members

--- Chapter4/ObjectLifeCycle/ObjectLifeCycle/main.cpp
+++ Chapter4/ObjectLifeCycle/ObjectLifeCycle/main.cpp
@@ -2,18 +2,35 @@
 //static storage duration
 //static or extern keywords
 //these variables have global scope:
-static int rat_things_power = 200;
+//static int rat_things_power = 200;
+//extern int rat_things_power = 200; //external linkage
 
 //automatic storage duration
 	//nuclear_isotopes and waste_heat are both automatic variables
 	//local variables with local scope
-void power_up_rat_thing(int nuclear_isotopes) {
-	rat_things_power = rat_things_power + nuclear_isotopes;
-	const auto waste_heat = rat_things_power * 20;
-	if (waste_heat > 10000) {
-		printf("Warning! Hot doggie!\n");
+//void power_up_rat_thing(int nuclear_isotopes) {
+//	rat_things_power = rat_things_power + nuclear_isotopes;
+//	const auto waste_heat = rat_things_power * 20;
+//	if (waste_heat > 10000) {
+//		printf("Warning! Hot doggie!\n");
+//	}
+//}
+
+//local static variables and static members:
+struct RatThing {
+	static int rat_things_power;
+	static void power_up_rat_thing(int nuclear_isotopes) {
+		rat_things_power = rat_things_power + nuclear_isotopes;
+		const auto waste_heat = rat_things_power * 20;
+		if (waste_heat > 10000) {
+			printf("Warning! Hot doggie!\n");
+		}
+		printf("Rat-thing power: %d\n", rat_things_power);
 	}
-}
+};
+
+//static members must be initialized globally:
+int RatThing::rat_things_power = 200;
 
 int main() {
 	//The object life cycle
@@ -27,12 +44,20 @@ int main() {
 	
 	//static storage duration
 	//static or extern keywords
-
-	printf("Rat-thing power: %d\n", rat_things_power);
+	//default is internal linkage: accessible to internal translation units
+	//external linkage: accessible to external translation units
+	/*printf("Rat-thing power: %d\n", rat_things_power);
 	power_up_rat_thing(100);
 	printf("Rat-thing power: %d\n", rat_things_power);
 	power_up_rat_thing(500);
 	printf("Rat-thing power: %d\n", rat_things_power);
+	*/
+	
+	//local static variables
+	//power_up_rat_thing(100);
+	//power_up_rat_thing(500);
 
-
+	//static members:
+	RatThing::power_up_rat_thing(100);
+	RatThing::power_up_rat_thing(500);
 }
