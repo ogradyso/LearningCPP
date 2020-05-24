1fac22aa8e440f5f753063f27c8fdf781a135389
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2020-05-23 20:07:12 -0500

    Dynamic arrays and exceptions

--- Chapter4/ObjectLifeCycle/ObjectLifeCycle/main.cpp
+++ Chapter4/ObjectLifeCycle/ObjectLifeCycle/main.cpp
@@ -1,4 +1,5 @@
 #include <cstdio>
+#include <stdexcept>
 //static storage duration
 //static or extern keywords
 //these variables have global scope:
@@ -29,8 +30,33 @@ struct RatThing {
 	}
 };
 
+//Tracing the object lifecycle
+struct Tracer {
+	Tracer(const char* name) : name{ name } {
+		printf("%s constructed.\n", name);
+	}
+	~Tracer() {
+		printf("%s destructed.\n", name);
+	}
+private:
+	const char* const name;
+};
+
+static Tracer t1{ "Static variable" };
+thread_local Tracer t2{ "Thread-local variable" };
+
 //static members must be initialized globally:
 int RatThing::rat_things_power = 200;
+
+//Exceptions:
+struct Groucho {
+	void forget(int x) {
+		if (x == 0xFACE) {
+			throw std::runtime_error{ "I'd be glad to make an exception." };
+		}
+		printf("Forgot 0x%x\n", x);
+	}
+};
 
 int main() {
 	//The object life cycle
@@ -60,4 +86,51 @@ int main() {
 	//static members:
 	RatThing::power_up_rat_thing(100);
 	RatThing::power_up_rat_thing(500);
+
+	// dynamic storage duration
+	//dynamic objects are called allocated objects and use the 'new' keyword
+	int* my_int_ptr = new int;
+	int* my_int_ptr2 = new int{ 42 }; //initializing dynamic object
+	//deallocation of dynamic objects uses 'delete' keyword
+	delete my_int_ptr;
+	delete my_int_ptr2;
+
+	//dynamic arrays format:
+	//new MyType[n_elements] {init-list}
+	//returns a pointer to the first element of the array
+	int* my_int_array_ptr = new int[100];
+	// or you can use variables:
+	int myNumber = { 42 };
+	int* my_int_array_ptr2 = new int[myNumber];
+
+	delete[] my_int_array_ptr2;
+	delete[] my_int_array_ptr;
+
+	//memory leaks
+	//always deallocate allocated objects
+	//if you don't you will cause memory leaks
+
+	//Tracing the object lifecycle
+	printf("A\n");
+	Tracer t3{ "Automatic variable" };
+	printf("B\n");
+	const auto* t4 = new Tracer{ "Dynamic variable" };
+	printf("C\n");
+
+	//Exceptions:
+	Groucho groucho;
+	try {
+		groucho.forget(0xC0DE);
+		groucho.forget(0xFACE);
+		groucho.forget(0xC0FFEE);
+	}
+	catch (const std::runtime_error & e) {
+		printf("exception caught with message: %s\n", e.what());
+	}
+	//stdlib exception classses
+
+
+
+
+
 }
