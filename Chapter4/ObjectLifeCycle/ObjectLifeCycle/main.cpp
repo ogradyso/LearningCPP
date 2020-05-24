123977344ea84927c76ca3d10bb9a806f7ccbaa3
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2020-05-24 08:11:57 -0500

    Exception handling and the simplestring classs

--- Chapter4/ObjectLifeCycle/ObjectLifeCycle/main.cpp
+++ Chapter4/ObjectLifeCycle/ObjectLifeCycle/main.cpp
@@ -1,5 +1,8 @@
 #include <cstdio>
 #include <stdexcept>
+#include <cstring>
+#include <exception>
+#pragma warning(disable: 4996)
 //static storage duration
 //static or extern keywords
 //these variables have global scope:
@@ -47,6 +50,41 @@ thread_local Tracer t2{ "Thread-local variable" };
 
 //static members must be initialized globally:
 int RatThing::rat_things_power = 200;
+
+//putting it all together, constructors, destructors, members, and exceptions
+struct SimpleString {
+	SimpleString(size_t max_size)
+		: max_size{ max_size },
+		length{} {
+		if (max_size == 0) {
+			throw std::runtime_error{ "Max size must be at least 1." };
+		}
+		buffer = new char[max_size];
+		buffer[0] = 0;
+	}
+	~SimpleString() {
+		delete[] buffer;
+	}
+
+	void print(const char* tag) const {
+		printf("%s: %s", tag, buffer);
+	}
+
+	bool append_line(const char* x) {
+		const auto x_len = strlen(x);
+		if (x_len + length + 2 > max_size) return false;
+		std::strncpy(buffer + length, x, max_size - length);
+		length += x_len;
+		buffer[length++] = '\n';
+		buffer[length] = 0;
+		return true;
+	}
+private:
+	size_t max_size;
+	char* buffer;
+	size_t length;
+};
+
 
 //Exceptions:
 struct Groucho {
@@ -127,10 +165,45 @@ int main() {
 	catch (const std::runtime_error & e) {
 		printf("exception caught with message: %s\n", e.what());
 	}
-	//stdlib exception classses
 
+	//stdlib exception classses and inheritance
+	try {
+		throw std::logic_error{ "It's not about who wrong "
+								"it's not about who right." };
+	}
+	catch (std::exception & ex) {
+		//Handles std::logic_error as it inherits from std::exception
+	}
 
+	//handle multiple exceptions with different results
+	try {
+		//code that might throw an exception
+	}
+	catch (const std::logic_error & ex) {
+		//log exception and terminate the program; there is a programming error!
+	}
+	catch (const std::runtime_error & ex) {
+		//do our best to recover gracefully
+	}
+	catch (const std::exception & ex) {
+		//this will handle any exception that derives from std::exception
+		//that is not a logic_error or a runtime_error
+	}
+	catch (...) {
+		//Panic; an unforeseen exception type was thrown
+	};
 
+	//putting it all together, constructors, destructors, members, and exceptions
+	SimpleString string{ 115 };
+	string.append_line( "Starbuck, whaddya hear?" );
+	string.append_line( "Nothing' but the rain." );
+	string.print("A: ");
+	string.append_line("Grab your gun and bring the cat in.");
+	string.append_line("Aye-aye sir, coming home.");
+	string.print("B: ");
+	if (!string.append_line("Galactica!")) {
+		printf("String was not big enough to append another message.");
+	}
 
 
 }
