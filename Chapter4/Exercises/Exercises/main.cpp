10f065009f2c657cc1d16e0edd22312d89b4ac18
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2020-05-25 18:53:27 -0500

    Fixed TimerClass to return the appropriate time upon destructor call.

--- Chapter4/Exercises/Exercises/main.cpp
+++ Chapter4/Exercises/Exercises/main.cpp
@@ -7,9 +7,9 @@
 struct TimerClass {
 	TimerClass(const char* myName)
 		: name{ myName } {
-		time_t timestamp;
-		timestamp = time(NULL);
-		//printf("Seconds since January 1, 1970 = %lld\n", ((((timestamp/60)/60)/24)/365));
+		//time_t timestamp;
+		time(&timestamp);
+		printf("Seconds since January 1, 1970 = %lld\n", ((((timestamp/60)/60)/24)/365));
 	}
 	//copy constructor:
 	TimerClass(const TimerClass& other) {
@@ -37,25 +37,37 @@ struct TimerClass {
 	}
 	~TimerClass() {
 		if (timestamp == 0) {
-			printf("%s has been moved. No age data available.", name);
+			//printf("%s has been moved. No age data available.", name);
 		}
 		else {
-			time_t timeOfDeath = time(0);
+			time_t timeOfDeath;
+			time(&timeOfDeath);
 			//printf("Seconds since January 1, 1970 = %lld\n", timeOfDeath);
-			long long ageOfObject = difftime(timeOfDeath, timestamp);
-			printf("%s was alive for %lld secs.\n", name, (long long)ageOfObject);
+			double ageOfObject = difftime(timeOfDeath, timestamp);
+			printf("%s was alive for %f secs.\n", name, ageOfObject);
 		}
 	}
+
 private:
 	time_t timestamp;
 	const char* name;
 };
 
+void someArbitraryMath() {
+	long long num1 = 50000000;
+	long long num2 = 0;
+	for (long long i = 0; i <= 50000; i++) {
+		for (long long j = 0; j <= 50000; j++) {
+			num2 = num2 + j;
+		}
+	}
+}
+
 int main() {
 	TimerClass myTimer("a");
 	TimerClass mySecondTimer("b");
+	someArbitraryMath();
 
-	mySecondTimer = std::move(myTimer);
-	return 0;
+	
 
 }
