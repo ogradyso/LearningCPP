5dd5ef94866fe71791030fd1105ac6489ba2a523
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2020-05-30 07:23:48 -0500

    template type deduction and an example class

--- Chapter_6/CompileTimePolymorphism/RuntimePolymorphism/listing_6_11.cpp
+++ Chapter_6/CompileTimePolymorphism/RuntimePolymorphism/listing_6_11.cpp
@@ -10,16 +10,16 @@ T mean(const T* values, size_t length) {
 	return result / length;
 }
 
-int main() {
-	const double nums_d[]{ 1.0,2.0,3.0,4.0 };
-	const auto result1 = mean<double>(nums_d, 4);
-	printf("double: %f\n", result1);
-
-	const float nums_f[]{ 1.0f, 2.0f, 3.0f, 4.0f };
-	const auto result2 = mean<float>(nums_f, 4);
-	printf("float: %f\n", result2);
-
-	const size_t nums_c[]{ 1, 2, 3, 4 };
-	const auto result3 = mean<size_t>(nums_c, 4);
-	printf("size_t: %zd\n", result3);
-}
+//int main() {
+//	const double nums_d[]{ 1.0,2.0,3.0,4.0 };
+//	const auto result1 = mean<double>(nums_d, 4);
+//	printf("double: %f\n", result1);
+//
+//	const float nums_f[]{ 1.0f, 2.0f, 3.0f, 4.0f };
+//	const auto result2 = mean<float>(nums_f, 4);
+//	printf("float: %f\n", result2);
+//
+//	const size_t nums_c[]{ 1, 2, 3, 4 };
+//	const auto result3 = mean<size_t>(nums_c, 4);
+//	printf("size_t: %zd\n", result3);
+//}
---
+++ Chapter_6/CompileTimePolymorphism/RuntimePolymorphism/listing_6_14.cpp
@@ -0,0 +1,27 @@
+template <typename>
+struct SimpleUniquePointer {
+	SimpleUniquePointer() = default;
+	SimpleUniquePointer(T* pointer)
+		: pointer{ pointer } {
+	}
+	~SimpleUniquePointer() {
+		if (pointer) delete pointer;
+	}
+	SimpleUniquePointer(const SimpleUniquePointer&) = delete;
+	SimpleUniquePointer& operator=(const SimpleUniquePointer&) = delete;
+	SimpleUniquePointer(SimpleUniquePointer&& other) no except
+		: pointer{ other.pointer } {
+		other.pointer = nullptr;
+	}
+	SimpleUniquePointer& operator=(SimpleUniquePointer&& other) noexcept {
+		if (pointer) delete pointer;
+		pointer = other.pointer;
+		other.pointer = nullptr;
+		return *this;
+	}
+	T* get() {
+		return pointer;
+	}
+private:
+	T* pointer;
+};
