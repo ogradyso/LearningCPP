28455b2c737546981f061db3505f38f6fa17f652
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2020-05-31 16:03:34 -0500

    Listing 6_25 and 6_26 will not compile unless origin is installed.

---
+++ Chapter_6/CompileTimePolymorphism/RuntimePolymorphism/listing_6_25.cpp
@@ -0,0 +1,8 @@
+//#include <origin/core/concepts.hpp>
+//size_t index_of_minimmum(Ordered* x, size_t length) {
+	//size_t min_index{};
+	//for (size_t i{ 1 }; i < length; i++) {
+	//	if (x[i] < x[min_index]) min_index = i;
+	//}
+	//return min_index;
+//}
