1a2df11785fee07d5b6ca283ee4225f07e05250f
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2020-06-03 07:01:51 -0500

    Type conversions

---
+++ Chapter_7/Expressions/Expressions/listing_7_12.cpp
@@ -0,0 +1,9 @@
+#include <limits>
+#include <cstdio>
+#include <cstdint>
+
+int main() {
+	int64_t b = std::numeric_limits<int64_t>::max();
+	int32_t c(b); //compiler complies because non-braced initialization allows for narrowing conversion
+	if (b != c) printf("Narrowing conversion!\n");
+}
---
+++ Chapter_7/Expressions/Expressions/listing_7_9.cpp
@@ -0,0 +1,9 @@
+#include <limits>
+#include <cstdio>
+
+//int main() {
+//	double x = std::numeric_limits<float>::max();
+//	long double y = std::numeric_limits<double>::max();
+//	float z = std::numeric_limits<long double>::max();
+//	printf("x: %g\ny: %Lg\nz: %g", x, y, z);
+//}
