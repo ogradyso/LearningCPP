63384fa3451d0892d90336d215975c57f93d8e4d
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2020-06-11 15:41:29 -0500

    Completed exercise 9-2

--- Chapter_9/Exercises/Ex_9_1_foldFunction.cpp
+++ Chapter_9/Exercises/Ex_9_1_foldFunction.cpp
@@ -8,16 +8,16 @@ constexpr Out fold(Fn function, In* input, size_t length, Out initial) {
 	return initial;
 }
 
-int main() {
-	int data[]{ 100, 200, 300, 400, 500 };
-	size_t data_len = 5;
-	auto sum = fold([](auto x, auto y) {return x + y; }, data, data_len, 0);
-	auto minNum = fold([](auto x, auto y) {return x < y ? x: y; }, data, data_len, 0);
-	auto maxNum = fold([](auto x, auto y) {return x < y ? y: x; }, data, data_len, 0);
-	auto numGreater = fold([](auto x, auto y) {return y > 200 ? x + 1 : 0; }, data, data_len, 0);
-	printf("Sum: %d\n", sum);
-	printf("Min: %d\n", minNum);
-	printf("Min: %d\n", maxNum);
-	printf("CountGreater than: %d\n", numGreater);
-
-}
+//int main() {
+//	int data[]{ 100, 200, 300, 400, 500 };
+//	size_t data_len = 5;
+//	auto sum = fold([](auto x, auto y) {return x + y; }, data, data_len, 0);
+//	auto minNum = fold([](auto x, auto y) {return x < y ? x: y; }, data, data_len, 0);
+//	auto maxNum = fold([](auto x, auto y) {return x < y ? y: x; }, data, data_len, 0);
+//	auto numGreater = fold([](auto x, auto y) {return y > 200 ? x + 1 : 0; }, data, data_len, 0);
+//	printf("Sum: %d\n", sum);
+//	printf("Min: %d\n", minNum);
+//	printf("Min: %d\n", maxNum);
+//	printf("CountGreater than: %d\n", numGreater);
+//
+//}
---
+++ Chapter_9/Exercises/Ex_9_2_commandLineHisto.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+#include <cstdint>
+#include <string>
+
+int main(int argc, char** argv) {
+	printf("Arguments: %d\n", argc);
+	for (size_t i{1}; i < argc; i++) {
+		printf("[%zd]:", i);
+		for (int j{ 0 }; j < strlen(argv[i]); j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	/*printf("Arguments: %d\n", argc);
+	for (size_t i{}; i < argc; i++) {
+		printf("%zd: %s\n", i, argv[i]);
+	}*/
+}
