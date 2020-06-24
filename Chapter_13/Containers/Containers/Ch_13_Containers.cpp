46a0ce788216c197e8dfa6d3799225ff2e48b736
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2020-06-23 21:04:43 -0500

    Exploring associative containers

--- Chapter_13/Containers/Containers/Ch_13_Containers.cpp
+++ Chapter_13/Containers/Containers/Ch_13_Containers.cpp
@@ -345,3 +345,145 @@ TEST_CASE("std::set allows access") {
 		REQUIRE(*pair_itr.second == 5);
 	}
 }
+
+TEST_CASE("std::set allows removal") {
+	std::set<int> fib{ 1,1,2,3,5 };
+	SECTION("with erase") {
+		fib.erase(3);
+		REQUIRE(fib.find(3) == fib.end());
+	}
+	SECTION("with clear") {
+		fib.clear();
+		REQUIRE(fib.empty());
+	}
+}
+
+TEST_CASE("std::multiset handles non-unique elements") {
+	std::multiset<int> fib{ 1,1,2,3,5 };
+	SECTION("as reflected by size") {
+		REQUIRE(fib.size() == 5);
+	}
+	SECTION("and count reeturns values greater than 1") {
+		REQUIRE(fib.count(1) == 2);
+	}
+	SECTION("and equal_range returns non-trivial ranges") {
+		auto [begin, end] = fib.equal_range(1);
+		REQUIRE(*begin == 1);
+		++begin;
+		REQUIRE(*begin == 1);
+		++begin;
+		REQUIRE(begin == end);
+	}
+}
+
+#include <functional>
+TEST_CASE("std::hash<long> returns") {
+	std::hash<long> hasher;
+	auto hash_code_42 = hasher(42);
+	SECTION("equal hash codes for equal types") {
+		REQUIRE(hash_code_42 == hasher(42));
+	}
+	SECTION("unequal hash ocdes for unequal keys") {
+		REQUIRE(hash_code_42 != hasher(43));
+	}
+}
+
+#include <functional>
+TEST_CASE("std::equal_to<long> returns") {
+	std::equal_to<long> long_equal_to;
+	SECTION("true when arguments equal") {
+		REQUIRE(long_equal_to(42, 42));
+	}
+	SECTION("false when arguments unequal") {
+		REQUIRE_FALSE(long_equal_to(42, 43));
+
+	}
+}
+
+#include <unordered_set>
+TEST_CASE("std::unordered_set") {
+	std::unordered_set<unsigned long> sheep(100);
+	SECTION("allows bucket count specification on construction") {
+		REQUIRE(sheep.bucket_count() >= 100);
+		REQUIRE(sheep.bucket_count() <= sheep.max_bucket_count());
+		REQUIRE(sheep.max_load_factor() == Approx(1.0));
+	}
+	SECTION("allows us to reserve space for elements") {
+		sheep.reserve(100'000);
+		sheep.insert(0);
+		REQUIRE(sheep.load_factor() <= 0.00001);
+
+		while (sheep.size() < 100'000)
+			sheep.insert(sheep.size());
+		REQUIRE(sheep.load_factor() <= 1.0);
+	}
+
+}
+
+#include <map>
+auto colour_of_magic = "Colour of magic";
+auto the_light_fantastic = "The Light Fantastic";
+auto equal_rites = "Equal Rites";
+auto mort = "Mort";
+
+TEST_CASE("std::map supports") {
+	SECTION("default construction") {
+		std::map<const char*, int> emp;
+		REQUIRE(emp.empty());
+	}
+	SECTION("braced initialization") {
+		std::map<const char*, int> pub_year{
+			{colour_of_magic, 1983},
+			{the_light_fantastic, 1986},
+			{equal_rites, 1987},
+			{mort, 1987}
+		};
+		REQUIRE(pub_year.size() == 4);
+	}
+}
+
+TEST_CASE("std::map is an associative array with") {
+	std::map<const char*, int> pub_year{
+		{colour_of_magic, 1983},
+		{the_light_fantastic, 1986},
+	};
+	SECTION("operator[]") {
+		REQUIRE(pub_year[colour_of_magic] == 1983);
+
+		pub_year[equal_rites] = 1987;
+		REQUIRE(pub_year[equal_rites] == 1987);
+
+		REQUIRE(pub_year[mort] == 0);
+	}
+	SECTION("an at method") {
+		REQUIRE(pub_year.at(colour_of_magic) == 1983);
+
+		REQUIRE_THROWS_AS(pub_year.at(equal_rites), std::out_of_range);
+	}
+}
+
+TEST_CASE("std::map supports insert") {
+	std::map<const char*, int> pub_year;
+	pub_year.insert({ colour_of_magic, 1983 });
+	REQUIRE(pub_year.size() == 1);
+
+	std::pair<const char*, int> tlfp{ the_light_fantastic, 1986 };
+	pub_year.insert(tlfp);
+	REQUIRE(pub_year.size() == 2);
+
+	auto [itr, is_new] = pub_year.insert({ the_light_fantastic, 9999 });
+	REQUIRE(itr->first == the_light_fantastic);
+	REQUIRE(itr->second == 1986);
+	REQUIRE_FALSE(is_new);
+	REQUIRE(pub_year.size() == 2);
+}
+
+TEST_CASE("std::map supports insert_or_assign") {
+	std::map<const char*, int> pub_year{
+		{the_light_fantastic, 9999}
+	};
+	auto [itr, is_new] = pub_year.insert_or_assign(the_light_fantastic, 1986);
+	REQUIRE(itr->second == 1986);
+	REQUIRE_FALSE(is_new);
+}
+
