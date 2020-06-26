1fc43b675d03877634e17b52afe619cffe55dd42
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2020-06-26 17:22:31 -0500

    Chapter 13 exercises completed

--- Chapter_13/Exercises/Exercises/Exercises/Ex_13_3_ArgAlphaSort.cpp
+++ Chapter_13/Exercises/Exercises/Exercises/Ex_13_3_ArgAlphaSort.cpp
@@ -1,8 +1,8 @@
-#define CATCH_CONFIG_MAIN
-#include "catch.h"
-#include <set>
-
-using namespace std;
+//#define CATCH_CONFIG_MAIN
+//#include "catch.h"
+//#include <set>
+//
+//using namespace std;
 
 //int char_type(const char x) {
 //	if (isupper(x)) {
@@ -24,28 +24,28 @@ using namespace std;
 //	else return char_type(x) > char_type(y);
 //}
 //
-struct CharStarCompare {
-	bool operator() (const char* lhs, const char* rhs) const {
-		int iteleft = 0;
-		int iteright = 0;
-		//compare each character in	 strings
-		while (lhs[iteleft] != NULL && rhs[iteright] != NULL) {
-			if (lhs[iteleft] < rhs[iteright]) return true;
-			if (lhs[iteleft] > rhs[iteright]) return false;
-			iteleft++;
-			iteright++;
-		}
-		return true;
-	}
-};
-
-
-int main(int argc, char** argv) {
-	set<const char*, CharStarCompare> arguments{};
-	for (size_t i{1}; i < argc; i++) {
-		arguments.insert(argv[i]);
-	}
-	for (auto iter = arguments.begin(); iter != arguments.end(); ++iter) {
-		cout << *iter << endl;
-	}
-}
+//struct CharStarCompare {
+//	bool operator() (const char* lhs, const char* rhs) const {
+//		int iteleft = 0;
+//		int iteright = 0;
+//		//compare each character in	 strings
+//		while (lhs[iteleft] != NULL && rhs[iteright] != NULL) {
+//			if (lhs[iteleft] < rhs[iteright]) return true;
+//			if (lhs[iteleft] > rhs[iteright]) return false;
+//			iteleft++;
+//			iteright++;
+//		}
+//		return true;
+//	}
+//};
+//
+//
+//int main(int argc, char** argv) {
+//	set<const char*, CharStarCompare> arguments{};
+//	for (size_t i{1}; i < argc; i++) {
+//		arguments.insert(argv[i]);
+//	}
+//	for (auto iter = arguments.begin(); iter != arguments.end(); ++iter) {
+//		cout << *iter << endl;
+//	}
+//}
---
+++ Chapter_13/Exercises/Exercises/Exercises/Ex_13_5_FibSumTimer.cpp
@@ -0,0 +1,65 @@
+//#define CATCH_CONFIG_MAIN
+//#include "catch.h"
+//
+//#include <chrono>
+//#include <cstdio>
+//#include <random>
+//#include <vector>
+//
+//
+//
+//long fib_sum(size_t n) {
+//	long last = 0, current = 1, temp = 0;
+//	if ((n == 0) | (n==1)) {
+//		return n;
+//	}
+//	for (int i = 0; i < n-1; i++) {
+//		temp = last+ current;
+//		last = current;
+//		current = temp;
+//	}
+//	return current;
+//
+//}
+//
+//long random() {
+//	static std::mt19937_64 mt_engine{ 102787 };
+//	static std::uniform_int_distribution<long> int_d{ 1000, 2000 };
+//	return int_d(mt_engine);
+//}
+//
+//struct Stopwatch {
+//	Stopwatch(std::chrono::nanoseconds& result)
+//		:result{result},
+//		start{std::chrono::system_clock::now()} {}
+//	~Stopwatch() {
+//		result = std::chrono::system_clock::now() - start;
+//	}
+//private:
+//	std::chrono::nanoseconds& result;
+//	const std::chrono::time_point<std::chrono::system_clock> start;
+//};
+//
+//long cached_fib_sum(const size_t& n) {
+//	static std::map<long, long> cache;
+//	//TODO: Implement me
+//	return 0;
+//}
+//
+//int main() {
+//	size_t samples{ 1'000'000 };
+//	std::vector<ULONG> ulMyVector{};
+//	std::chrono::nanoseconds elapsed;
+//	{
+//		Stopwatch stopwatch{ elapsed };
+//		volatile double answer;
+//		while (samples--) {
+//			answer = fib_sum(random());
+//			//answer = cached_fib_sum(random());
+//	printf("Answer %d\n", answer);
+//		}
+//		printf("Answer: %f\n", answer);
+//	}
+//	printf("Elapsed: %g s.\n", elapsed.count() / 1'000'000'000);
+//
+//}
