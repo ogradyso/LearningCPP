ca0d4397b5e1a3d5c217257ee3c121ec4ea75a93
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2020-07-01 06:58:32 -0500

    Boost string algorithms

--- Chapter_15/StringProcessing/StringProcessing/StringProcessing.cpp
+++ Chapter_15/StringProcessing/StringProcessing/StringProcessing.cpp
@@ -327,3 +327,70 @@ TEST_CASE("std::basic_regex constructs from a string literal") {
 	std::regex zip_regex{ R"((\w{2})?(\d{5})(-\d{4})?)" };
 	REQUIRE(zip_regex.mark_count() == 3);
 }
+
+//algorithms: matching
+#include <regex>
+#include <string>
+
+TEST_CASE("std::sub_match") {
+	std::regex regex{ R"((\w{2})(\d{5})(-\d{4})?)" };
+	std::smatch results;
+	SECTION("returns true given matching string") {
+		std::string zip("NJ07936-3173");
+		const auto matched = std::regex_match(zip, results, regex);
+		REQUIRE(matched);
+		REQUIRE(results[0] == "NJ07936-3173");
+		REQUIRE(results[1] == "NJ");
+		REQUIRE(results[2] == "07936");
+		REQUIRE(results[3] == "-3173");
+	}
+	SECTION("returns false given non-matching string") {
+		std::string zip("Iomega Zip 100");
+		const auto matched = std::regex_match(zip, results, regex);
+		REQUIRE_FALSE(matched);
+	}
+}
+
+//algorithms searching
+TEST_CASE("when only part of a string matches a regex, std::regex_ ") {
+	std::regex regex{ R"((\w{2})(\d{5})(-\d{4}) ? )" };
+	std::string sentence("The string NJ07936-3173 is a Zip code.");
+	SECTION("match returns false") {
+		REQUIRE_FALSE(std::regex_match(sentence, regex));
+	}
+	SECTION("search returns true") {
+		REQUIRE(std::regex_search(sentence, regex));
+	}
+}
+
+//algorithms replacing
+TEST_CASE("std::regex_replace") {
+	std::regex regex{ "[aeoiu]" };
+	std::string phrase("queueing and cooeeing in eutopia");
+	const auto result = std::regex_replace(phrase, regex, "_");
+	REQUIRE(result == "q_____ng _nd c_____ng _n __t_p__");
+}
+
+//Boost::string algorithms
+
+//boost::string predicates
+#include <string>
+#include <boost/algorithm/string/predicate.hpp>
+
+TEST_CASE("boost::algorith") {
+	using namespace boost::algorithm;
+	using namespace std::literals::string_literals;
+	std::string word("cymotrichous");
+	SECTION("starts_with tests a string's beginning") {
+		REQUIRE(starts_with(word, "cymo"s));
+	}
+	SECTION("istarts_with is case insensitive") {
+		REQUIRE(istarts_with(word, "cYmO"s));
+	}
+}
+
+TEST_CASE("boost::algorithm::all evalueates a predicate for all elements") {
+	using namespace boost::algorithm;
+	std::string word("juju");
+	REQUIRE(all(word, [](auto c) {return c == 'j' || c == 'u'; }));
+}
