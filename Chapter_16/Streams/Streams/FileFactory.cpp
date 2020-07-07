4ac63b443f95409154438cdb9cd18b6f9232f261
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2020-07-07 06:56:18 -0500

    Exploring file streams

---
+++ Chapter_16/Streams/Streams/FileFactory.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+ifstream open(const char* path, ios_base::openmode mode = ios_base::in) {
+	ifstream file{ path, mode };
+	if (!file.is_open()) {
+		string err{ "Unable to open file " };
+		err.append(path);
+		throw runtime_error{ err };
+	}
+	file.exceptions(ifstream::badbit);
+	return file;
+}
+
