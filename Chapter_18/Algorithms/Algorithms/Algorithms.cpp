cd54655d79a3aa2523d9fe4f55153cbefdc446c1
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2020-07-17 07:00:17 -0500

    Continued exploring non-modifying algorithms

--- Chapter_18/Algorithms/Algorithms/Algorithms.cpp
+++ Chapter_18/Algorithms/Algorithms/Algorithms.cpp
@@ -48,3 +48,102 @@ TEST_CASE("none_of") {
 	};
 	REQUIRE_FALSE(none_of(words.cbegin(), words.cend(), is_definite_article));
 }
+
+//for_each
+TEST_CASE("for_each") {
+	vector<string> words{ "David", "Donald","Doo" };
+	size_t number_of_Ds{};
+	const auto count_Ds = [&number_of_Ds](const auto& word) {
+		if (word.empty()) return;
+		if (word[0] == 'D') ++number_of_Ds;
+	};
+	for_each(words.cbegin(), words.cend(), count_Ds);
+	REQUIRE(3 == number_of_Ds);
+}
+
+//for_each_n
+TEST_CASE("for_each_n") {
+	vector<string> words{ "ear", "egg","elephant" };
+	size_t characters{};
+	const auto count_characters = [&characters](const auto& word) {
+		characters += word.size();
+	};
+	for_each_n(words.cbegin(), words.size(), count_characters);
+	REQUIRE(14 == characters);
+}
+
+
+//find, find_if, and find_if_not
+TEST_CASE("find find_if, find_if_not") {
+	vector<string> words{ "fiffer","feffer","feff" };
+	const auto find_result = find(words.cbegin(), words.cend(), "feff");
+	REQUIRE(*find_result == words.back());
+
+	const auto defends_digital_privacy = [](const auto& word) {
+		return string::npos != word.find("eff");
+	};
+	const auto find_if_result = find_if(words.cbegin(), words.cend(),
+		defends_digital_privacy);
+	REQUIRE(*find_if_result == "feffer");
+
+	const auto find_if_not_result = find_if_not(words.cbegin(), words.cend(),
+		defends_digital_privacy);
+	REQUIRE(*find_if_not_result == words.front());
+}
+
+//find_end
+TEST_CASE("find_end") {
+	vector<string> words1{ "Goat","girl","googoo","goggles" };
+	vector<string> words2{ "girl", "googoo" };
+	const auto find_end_result1 = find_end(words1.cbegin(), words1.cend(),
+		words2.cbegin(), words2.cend());
+	REQUIRE(*find_end_result1 == words1[1]);
+
+	const auto has_length = [](const auto& word, const auto& len) {
+		return word.length() == len;
+	};
+	vector<size_t> sizes{ 4,6 };
+	const auto find_end_result2 = find_end(words1.cbegin(), words1.cend(),
+		sizes.cbegin(), sizes.cend(),
+		has_length);
+	REQUIRE(*find_end_result2 == words1[1]);
+}
+
+//find_first
+TEST_CASE("find_first_of") {
+	vector<string> words{ "Hen","in","a","hat" };
+	vector<string> indefinite_articles{ "a","an" };
+	const auto find_first_of_result = find_first_of(words.cbegin(),
+		words.cend(),
+		indefinite_articles.cbegin(),
+		indefinite_articles.cend());
+	REQUIRE(*find_first_of_result == words[2]);
+}
+
+//adjacent_find
+TEST_CASE("adjacent_find") {
+	vector<string> words{ "Icabod","is", "itchy" };
+	const auto first_letters_match = [](const auto& word1, const auto& word2) {
+		if (word1.empty() || word2.empty()) return false;
+		return word1.front() == word2.front();
+	};
+	const auto adjacent_find_result = adjacent_find(words.cbegin(), words.cend(),
+		first_letters_match);
+	REQUIRE(*adjacent_find_result == words[1]);
+}
+
+//count
+TEST_CASE("count") {
+	vector<string> words{ "jelly", "jar","and", "jam" };
+	const auto n_ands = count(words.cbegin(), words.cend(), "and");
+	REQUIRE(n_ands == 1);
+
+	const auto contains_a = [](const auto& word) {
+		return word.find('a') != string::npos;
+	};
+	const auto count_if_result = count_if(words.cbegin(), words.cend(), contains_a);
+	REQUIRE(count_if_result == 3);
+}
+
+//mismatch
+TEST_CASE("")
