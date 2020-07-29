229fc753cece0f25b2e3dd2c9ac15fceb76add9b
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2020-07-29 07:44:31 -0500

    Starting chapter 20 Networking with BoostASIO

---
+++ Chapter_19/Exercises/Exercises/Ex_19_3_ThreadSafeQueue.cpp
@@ -0,0 +1,16 @@
+//#include <future>
+//#include <iostream>
+//#include <mutex>
+//#include <queue>
+//
+//using namespace std;
+//
+//template<class T, class Container = std::deque<T>>
+//class ThreadSafeQueue {
+//	ThreadSafeQueue() {
+//		internalQueue{}
+//	};
+//private:
+//	queue<T> internalQueue{};
+//};
+//
