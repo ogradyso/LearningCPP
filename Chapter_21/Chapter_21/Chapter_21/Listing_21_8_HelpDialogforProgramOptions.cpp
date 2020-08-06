5410ebe417cb579a8f0fa6a76af51098fb87a7d7
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2020-08-06 07:05:25 -0500

    Exploring command line argument parsing

--- Chapter_21/Chapter_21/Chapter_21/Listing_21_8_HelpDialogforProgramOptions.cpp
+++ Chapter_21/Chapter_21/Chapter_21/Listing_21_8_HelpDialogforProgramOptions.cpp
@@ -15,5 +15,41 @@ int main(int argc, char** argv) {
 		("pattern", value<std::string>(), "pattern to search for")
 		("paths", value<std::vector<std::string>>(), "path to search");
 	std::cout << description;
+	
+	// listing 21_9: adding positional arguments:
+	positional_options_description positional;
+	positional.add("pattern", 1);
+	positional.add("path", -1);
+	
+	//listing_21_10: adding command line parser:
+	command_line_parser parser{ argc, argv };
+	parser.options(description);
+	parser.positional(positional);
+	auto parsed_result = parser.run();
 
+	//listing 21_11: Parsing arguments into a variable map
+	variables_map vm;
+	store(parsed_result, vm);
+	notify(vm);
+
+	//listing 21 12: retrieving variables from a variables_map
+	if (is_help) std::cout << "Is help.\n";
+	if (is_recursive) std::cout << "Is recursive.\n";
+	std::cout << "Threads: " << vm["threads"].as<int>() << "\n";
+	if (!vm["pattern"].empty()) {
+		std::cout << "Pattern: " << vm["pattern"].as<std::string>() << "\n";
+	}
+	else {
+		std::cout << "Empty pattern.\n";
+	}
+	if (!vm["path"].empty()) {
+		std::cout << "Paths:\n";
+		for (const auto& path : vm["path"].as<std::vector<std::string>>())
+			std::cout << "\t" << path << "\n";
+	}
+	else {
+		std::cout << "Empty path.\n";
+	}
 }
+
+
