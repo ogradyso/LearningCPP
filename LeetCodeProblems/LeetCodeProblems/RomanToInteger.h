43f1914be38fb4babbc3aae1458316fe0e5ea384
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2020-08-13 07:01:15 -0500

    Complete palindromeNumber, started romanToInteger

--- LeetCodeProblems/LeetCodeProblems/RomanToInteger.h
+++ LeetCodeProblems/LeetCodeProblems/RomanToInteger.h
@@ -1,29 +1,52 @@
-﻿#pragma once
+#pragma once
 #define CATCH_CONFIG_MAIN
 #include "catch.h"
 
 // includes for Leet submission:
 
 
-//Determine whether an integer is a palindrome.An integer is a palindrome when it reads the same backward as forward.
+
+//Roman numerals are represented by seven different symbols : I, V, X, L, C, Dand M.
+//
+//Symbol       Value
+//I             1
+//V             5
+//X             10
+//L             50
+//C             100
+//D             500
+//M             1000
+//For example, two is written as II in Roman numeral, just two one's added together. Twelve is written as, XII, which is simply X + II. The number twenty seven is written as XXVII, which is XX + V + II.
+//
+//Roman numerals are usually written largest to smallest from left to right.However, the numeral for four is not IIII.Instead, the number four is written as IV.Because the one is before the five we subtract it making four.The same principle applies to the number nine, which is written as IX.There are six instances where subtraction is used :
+//
+//I can be placed before V(5) and X(10) to make 4 and 9.
+//X can be placed before L(50) and C(100) to make 40 and 90.
+//C can be placed before D(500) and M(1000) to make 400 and 900.
+//Given a roman numeral, convert it to an integer.Input is guaranteed to be within the range from 1 to 3999.
 //
 //Example 1:
 //
-//Input: 121
-//Output : true
+//Input: "III"
+//Output : 3
 //Example 2 :
 //
-//    Input : -121
-//    Output : false
-//    Explanation : From left to right, it reads - 121. From right to left, it becomes 121 - .Therefore it is not a palindrome.
-//    Example 3 :
+//	Input : "IV"
+//	Output : 4
+//	Example 3 :
 //
-//    Input : 10
-//    Output : false
-//    Explanation : Reads 01 from right to left.Therefore it is not a palindrome.
-//    Follow up :
+//	Input : "IX"
+//	Output : 9
+//	Example 4 :
 //
-//Coud you solve it without converting the integer to a string ?
+//	Input : "LVIII"
+//	Output : 58
+//	Explanation : L = 50, V = 5, III = 3.
+//	Example 5 :
+//
+//	Input : "MCMXCIV"
+//	Output : 1994
+//	Explanation : M = 1000, CM = 900, XC = 90 and IV = 4.
 
 class Solution {
 public:
@@ -31,7 +54,7 @@ public:
         int nextDigit;
         int reversed = 0;
         int copyX = x;
-        if (x >= 0) 
+        if (x >= 0)
         {
             while (x > 0)
             {
@@ -41,16 +64,16 @@ public:
                 if (reversed < INT_MIN / 10 || (reversed == INT_MIN / 10 && nextDigit < -8)) return false;
                 reversed = reversed * 10 + nextDigit;
             }
-            if (copyX == reversed) 
+            if (copyX == reversed)
             {
                 return true;
             }
-            else 
+            else
             {
                 return false;
             }
         }
-        return {false};
+        return { false };
     }
     bool isPalindromeLeetAnswer(int x) {
         if (x < 0 || (x % 10 == 0 && x != 0)) return false;
@@ -74,11 +97,11 @@ Solution mySolution{};
 
 TEST_CASE("isPalindrome ") {
     SECTION("passess the initial examples") {
-        int numberInput1{121};
+        int numberInput1{ 121 };
         bool answer1{ true };
         REQUIRE(mySolution.isPalindromeLeetAnswer(numberInput1) == answer1);
         int numberInput2{ -121 };
-        bool answer2{ false};
+        bool answer2{ false };
         REQUIRE(mySolution.isPalindromeLeetAnswer(numberInput2) == answer2);
         int numberInput3{ 10 };
         bool answer3{ false };
@@ -89,7 +112,7 @@ TEST_CASE("isPalindrome ") {
         bool answer4 = { true };
         REQUIRE(mySolution.isPalindromeLeetAnswer(numberInput4) == answer4);
         int numberInput5{};
-        int answer5 = {true};
+        int answer5 = { true };
         REQUIRE(mySolution.isPalindromeLeetAnswer(numberInput5) == answer5);
     }
     SECTION("works with integer consisting of the same number or a pattern") {
