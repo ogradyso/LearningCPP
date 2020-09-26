7e3c02b508ee73cbea9ca6c470d5a27f5ed7cc22
Shaun O'Grady <shaun.m.ogrady@gmail.com>
2020-09-26 07:38:34 -0500

    Created lesson prompt parser!

--- ParseXML/ParseXML/ParseXML/ParseQuestLesson.cpp
+++ ParseXML/ParseXML/ParseXML/ParseQuestLesson.cpp
@@ -13,28 +13,27 @@
 std::vector<std::string> getLessonPrompts(const char* gameFilePath) {
 	tinyxml2::XMLDocument xmlDoc;
 
-	tinyxml2::XMLError eResult = xmlDoc.LoadFile(gameFilePath);
+	tinyxml2::XMLError eResult = xmlDoc.LoadFile("Lesson1.xml");
 
 	//XMLCheckResult(eResult);
 
 	//xtracting data from an XMLDocument:
-	tinyxml2::XMLNode* pRoot = xmlDoc.FirstChild();
-
-	//if (pRoot == nullptr) std::cout << tinyxml2::XML_ERROR_FILE_READ_ERROR << "\n"; return "Error Loading XML file";
-
+	tinyxml2::XMLNode* pRoot = xmlDoc.FirstChildElement("HelloWorld");
+	std::vector<std::string> vecList;
+	//if (pRoot == nullptr) std::cout << tinyxml2::XML_ERROR_FILE_READ_ERROR << "\n"; vecList.push_back("Error"); return vecList;
+	
 	//get the elements of a list: 
 	//requires a vector in the XML document:
 
 	//tinyxml2::XMLElement* pElement = pRoot->FirstChildElement("LessonPrompts");
 	//if (pElement == nullptr) return tinyxml2::XML_ERROR_PARSING_ELEMENT;
 
-	tinyxml2::XMLElement *pListElement = pRoot->FirstChildElement("LessonPrompts");
-	std::vector<std::string> vecList;
+	tinyxml2::XMLElement* pListElement = pRoot->FirstChildElement("LessonPrompts")->FirstChildElement("Prompt");
 
 	while (pListElement != nullptr)
 	{
-		std::string prompt;
-		prompt = pListElement->GetText();
+		const char* prompt = pListElement->GetText();
+		std::string strPrompt{prompt};
 		//XMLCheckResult(eResult);
 
 		vecList.push_back(prompt);
@@ -55,6 +54,17 @@ int main() {
 	{
 		std::cout << prompt << "\n";
 	}
+	/*tinyxml2::XMLDocument xmlDoc;
+
+	tinyxml2::XMLError eResult = xmlDoc.LoadFile("Lesson1.xml");
+	if (eResult != tinyxml2::XML_SUCCESS) std::cout << "Failed at load";
+
+	tinyxml2::XMLNode* pRoot = xmlDoc.FirstChildElement("HelloWorld");
+	if (pRoot == nullptr) std::cout << "Failed to get root element.";
+
+	tinyxml2::XMLElement* pListElement = pRoot->FirstChildElement("LessonPrompts");
+	if (pListElement == nullptr) std::cout << "Failed to get list element.";*/
+
 }
 
 
